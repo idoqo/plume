7520424aacdf62863c8792835345a213c550a452
idoko <radpangolin@gmail.com>
2019-11-19 21:00:35 +0100

    setup authentication wall

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -299,10 +299,10 @@ Devise.setup do |config|
   config.jwt do |jwt|
     jwt.secret = ENV['DEVISE_SECRET_KEY']
     jwt.dispatch_requests = [
-        ['POST', %r{^/login$}]
+        ['POST', %r{^/v1/auth/login$}]
     ]
     jwt.revocation_requests = [
-        ['POST', %r{^/logout$}]
+        ['POST', %r{^/v1/auth/logout$}]
     ]
 
     jwt.expiration_time = 30.minutes.to_i
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  namespace :v1 do
+  scope :v1, defaults: {format: :json} do
     devise_for :users,
                path: 'auth',
                path_names: {
