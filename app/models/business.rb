693720701796a5bc1b95fdb5cc4a15c7c7e5484b
idoko <radpangolin@gmail.com>
2019-12-24 12:12:55 +0100

    setup migrations/seeders for categories

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,5 +1,6 @@
 class Business < ApplicationRecord
   belongs_to :user
+  belongs_to :category
 
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
---
+++ app/models/category.rb
@@ -0,0 +1,5 @@
+class Category < ApplicationRecord
+  has_many :businesses
+
+  validates :name, :presence => true
+end
