1e9a6e93425665b90d278fcae326f3f75aea1aa2
idoko <radpangolin@gmail.com>
2020-01-31 05:44:58 +0100

    bumped gems, configured dockerignore

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,11 @@
 set -e
 
 # remove pre-existing server.pid to fix rails restart issue
-rm -f /pidgin/tmp/pids/server.pid
+if [[ "$@" == *"rails s"* ]]; then
+  if [ -f /plume/tmp/pids/server.pid ]; then
+    rm -f /plume/tmp/pids/server.pid
+  fi
+fi
 
 # execute the container's main process
 
